82566a7e710e6cf65a06d540f8c6433e0db02798
youdongz <youdongz@berkeley.edu>
2017-10-27 16:02:35 -0700

    Krippendorff analysis fixed

--- tt/src/main/java/goodly/app/App.java
+++ tt/src/main/java/goodly/app/App.java
@@ -1,4 +1,5 @@
-package goodly.app;
+ package goodly.app;
+
 import org.json.simple.parser.ParseException;
 import org.dkpro.statistics.agreement.unitizing.*;
 import java.io.*;
@@ -9,20 +10,17 @@ import org.json.simple.parser.JSONParser;
 
 public class App {
 	public static void main(String[] args) {
-		// UnitizingAnnotationStudy study = makeStudy("same_1.json", "same_2.json");	
 	        UnitizingAnnotationStudy study1 = makeStudy("same_1.json", "same_2.json");	
 	        UnitizingAnnotationStudy study2 = makeStudy("different_1.json", "different_2.json");	
 		// testStudy();
 		UnitizingAnnotationStudy[] studies = {study1, study2};
 		for (UnitizingAnnotationStudy study : studies) {
+			System.out.println("-----");
 			KrippendorffAlphaUnitizingAgreement alpha = new KrippendorffAlphaUnitizingAgreement(study);
 			for (Object category : study.getCategories()) {
 		    		System.out.print(category + ": ");
-		    		System.out.print(alpha.calculateCategoryAgreement(category) + ", ");
-		    	System.out.println(alpha.calculateCategoryAgreement(category));
+		    		System.out.println(alpha.calculateCategoryAgreement((String) category));
 			}
-			System.out.println(study.getUnits());
-			System.out.println("-----");
 		}
 	}
 
@@ -39,12 +37,14 @@ public class App {
         public static UnitizingAnnotationStudy makeStudy(String filename1, String filename2) {
 		JSONParser parser = new JSONParser();
 	        try {     
-                    JSONObject same_a = (JSONObject) parser.parse(new FileReader(new File(filename1)));
-                    JSONObject same_b = (JSONObject) parser.parse(new FileReader(new File(filename2)));
+                    JSONObject a = (JSONObject) parser.parse(new FileReader(new File(filename1)));
+                    JSONObject b = (JSONObject) parser.parse(new FileReader(new File(filename2)));
 		    
-		    JSONObject[] jsons = {same_a, same_b};		    
-		    for (JSONObject json: jsons) {
-                        UnitizingAnnotationStudy study = new UnitizingAnnotationStudy(2, ((String) json.get("text")).length());
+		    JSONObject jsons[] = {a, b};		    
+                    UnitizingAnnotationStudy study = new UnitizingAnnotationStudy(2, ((String) a.get("text")).length());
+		    for (int i = 0; i < jsons.length; i++) {
+			JSONObject json = jsons[i];
+			int increment = i;
 
                         JSONArray runs = (JSONArray) json.get("highlight_taskruns");
 		        JSONObject first_run = (JSONObject) runs.get(0);
@@ -59,11 +59,12 @@ public class App {
 				JSONArray offset = (JSONArray) of;
                                 Long start = (Long) offset.get(0);
                                 Long end = (Long) offset.get(1);
-                                study.addUnit(start.intValue(), end.intValue() - start.intValue(), contributor.intValue(), topic);
+				int annotator = increment; // contributor.intValue() + increment;
+                                study.addUnit(start.intValue(), end.intValue() - start.intValue(), annotator, topic);
 			    }
                         }
-		        return study;
 		    }
+		    return study;
 		} catch (FileNotFoundException e) {
             	    e.printStackTrace();
         	} catch (IOException e) {
